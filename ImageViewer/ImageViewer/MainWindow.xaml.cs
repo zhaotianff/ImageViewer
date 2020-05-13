c2c5b24ca9c105a2183cb6ebac8bde9ae973b51b
zhaotianff <zhaotianff@DESKTOP-VPOI8TT>
2020-05-13 11:14:11 +0800

    basic

--- ImageViewer/ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/ImageViewer/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Dicom;
+using Dicom.Imaging;
+using System.Runtime.InteropServices;
 
 namespace ImageViewer
 {
@@ -23,6 +27,51 @@ namespace ImageViewer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Dicom Files|*.dcm";
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            if(openFileDialog.ShowDialog() == true)
+            {
+                this.image.Source = GetImageSource(openFileDialog.FileName);
+            }
+        }
+
+        private WriteableBitmap GetWriteableBitmap(string fileName)
+        {
+            var image = new DicomImage(fileName);
+
+            //I need get raw data here
+#pragma warning disable CS0618
+            byte[] rawPixelData = image.PixelData.GetFrame(0).Data;
+
+            WriteableBitmap writeableBitmap = new WriteableBitmap(image.Width, image.Height, 96.0, 96.0, PixelFormats.Gray8, BitmapPalettes.BlackAndWhite);
+
+            //todo I should convert 12/16 bit image to 8-bit           
+            writeableBitmap.Lock();
+            Marshal.Copy(rawPixelData, 0, writeableBitmap.BackBuffer, image.Width * image.Height);
+            Int32Rect int32Rect = new Int32Rect(0, 0, image.Width, image.Height);
+            writeableBitmap.AddDirtyRect(int32Rect);
+            writeableBitmap.Unlock();
+            return writeableBitmap;
+        }
+
+        private ImageSource GetImageSource(string fileName)
+        {
+            var writeableBitmap = GetWriteableBitmap(fileName);
+            TransformedBitmap bitmap = new TransformedBitmap();
+            bitmap.BeginInit();
+            bitmap.Source = writeableBitmap;
+            bitmap.EndInit();
+            return bitmap;
         }
     }
 }
